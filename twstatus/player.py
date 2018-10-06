48c5421d5bae970c1ad92b61eebe7e6b2cacc823
Edgar <git@edgarluque.com>
2018-10-06 21:01:55 +0200

    first commit

---
+++ twstatus/player.py
@@ -0,0 +1,12 @@
+class Player:
+    def __init__(self,
+                 name: str,
+                 clan: str,
+                 country: int,
+                 score: int,
+                 is_spectator: bool):
+        self.name = name
+        self.clan = clan
+        self.country = country
+        self.score = score
+        self.is_spectator = is_spectator
---
+++ twstatus/serverinfo.py
@@ -0,0 +1,33 @@
+from typing import List
+from .player import Player
+
+
+class ServerInfo:
+    def __init__(self,
+                 version: str=None,
+                 name: str=None,
+                 _map: str=None,
+                 game_type: str=None,
+                 password: bool=None,
+                 player_count: int=None,
+                 max_player_count: int=None,
+                 client_count: int=None,
+                 max_client_count: int=None,
+                 mapcrc: int=None,
+                 mapsize: int=None,
+                 players: List[Player]=None):
+        self.name = name
+        self.map = _map
+        self.version = version
+        self.game_type = game_type
+        self.password = password
+        self.player_count = player_count
+        self.max_player_count = max_player_count
+        self.client_count = client_count
+        self.max_client_count = max_client_count
+        self.mapcrc = mapcrc
+        self.mapsize = mapsize
+        self.players = players
+
+        if self.players is None:
+            self.players: List[Player] = []
